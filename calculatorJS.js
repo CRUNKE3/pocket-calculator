f0e8e8c1dd05a177a74420c4d93bc6b923232ed1
Damian Czernikowski <damianczern@gmail.com>
2018-12-20 12:39:32 -0500

    the beginning

---
+++ calculatorJS.js
@@ -0,0 +1,41 @@
+function display() {}
+
+function number1() {}
+
+function number2() {}
+
+function number3() {}
+
+function number4() {}
+
+function number5() {}
+
+function number6() {}
+
+function number7() {}
+
+function number8() {}
+
+function number9() {}
+
+function number0() {}
+
+function addition() {}
+
+function subtraction() {}
+
+function multiplication() {}
+
+function division() {}
+
+function equals() {}
+
+function negation() {}
+
+function percentage() {}
+
+function clear() {}
+
+function squareRoot() {}
+
+function exponent() {}
