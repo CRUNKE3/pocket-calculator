858d2bc3fef455731846875ecb5055bcf9f4e485
Damian Czernikowski <damianczern@gmail.com>
2019-01-07 12:37:45 -0500

    fixed the function problem in JS

--- calculatorJS.js
+++ calculatorJS.js
@@ -3,9 +3,9 @@ function display() {
 }
 
 function number1() {
-  var div = document.getElementById("display");
+  var p = document.getElementById("display");
 
-  div.innerHTML = "1"
+  p.innerHTML = "1";
 }
 
 function number2() {
@@ -14,36 +14,36 @@ function number2() {
 
 function number3() {var div = document.getElementById("display");}
 
-function number4() {var div = document.getElementById="display";}
+function number4() {var div = document.getElementById("display");}
 
-function number5() {var div = document.getElementById="display";}
+function number5() {var div = document.getElementById("display");}
 
-function number6() {var div = document.getElementById="display";}
+function number6() {var div = document.getElementById("display");}
 
-function number7() {var div = document.getElementById="display";}
+function number7() {var div = document.getElementById("display");}
 
-function number8() {var div = document.getElementById="display";}
+function number8() {var div = document.getElementById("display");}
 
-function number9() {var div = document.getElementById="display";}
+function number9() {var div = document.getElementById("display");}
 
-function number0() {var div = document.getElementById="display";}
+function number0() {var div = document.getElementById("display");}
 
-function addition() {var div = document.getElementById="display";}
+function addition() {var div = document.getElementById("display");}
 
-function subtraction() {var div = document.getElementById="display";}
+function subtraction() {var div = document.getElementById("display");}
 
-function multiplication() {var div = document.getElementById="display";}
+function multiplication() {var div = document.getElementById("display");}
 
-function division() {var div = document.getElementById="display";}
+function division() {var div = document.getElementById("display");}
 
-function equals() {var div = document.getElementById="display";}
+function equals() {var div = document.getElementById("display");}
 
-function negation() {var div = document.getElementById="display";}
+function negation() {var div = document.getElementById("display");}
 
-function percentage() {var div = document.getElementById="display";}
+function percentage() {var div = document.getElementById("display");}
 
-function clear() {var div = document.getElementById="display";}
+function clear() {var div = document.getElementById("display");}
 
-function squareRoot() {var div = document.getElementById="display";}
+function squareRoot() {var div = document.getElementById("display");}
 
-function exponent() {var div = document.getElementById="display";}
+function exponent() {var div = document.getElementById("display");}
