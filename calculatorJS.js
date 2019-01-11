826c32efb3e20d277ff2a9fdcdff8269c05179b4
Damian Czernikowski <damianczern@gmail.com>
2019-01-11 13:04:44 -0500

    apparently the variable of numer_uno isn't defined in the equals function. Must fix somehow

--- calculatorJS.js
+++ calculatorJS.js
@@ -1,3 +1,28 @@
+let additionStatus = false;
+
+
 function number(num) {
   document.calc.display.value = document.calc.display.value + num;
+}
+
+function clearbutton() {
+  document.calc.display.value = "";
+  additionStatus = false;
+  numer_ouno="";
+  numero_dos="";
+}
+
+function addition() {
+  let numero_uno = document.calc.display.value;
+  let additionStatus = true;
+  document.calc.display.value = "";
+}
+
+function equals() {
+
+  let numero_dos = document.calc.display.value;
+  if (additionStatus=true) {
+    let displayAnswer = numero_uno+numero_dos;
+    document.calc.display = displayAnswer;
+  }
 }
